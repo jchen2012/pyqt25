cc35115dec1de71e4599c218f3713ea7c1937617
Ji Chen <jchen@tactilemedical.com>
2025-12-15 15:50:32 -0600

    signals and slots

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sys
 
+from random import choice
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import (
 QApplication,
@@ -7,22 +8,41 @@ QMainWindow,
 QPushButton,
 )
 
+window_titles = [
+"My App",
+"Still My App",
+"What on earth",
+"This is surprising",
+"Something went wrong",
+]
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-        self.setFixedSize(QSize(400, 300))
+        self.button = QPushButton("Press Me!")
+        # self.setFixedSize(QSize(400, 300))
+        self.setMinimumSize(QSize(400, 300))
+        self.setMaximumSize(QSize(800, 600))
 
-        button.clicked.connect(self.the_button_was_clicked)
+        self.button.clicked.connect(self.the_button_was_clicked)
+        
+        self.windowTitleChanged.connect(self.the_window_title_changed)
+                
         # Set the central widget of the Window.
-        self.setCentralWidget(button)
+        self.setCentralWidget(self.button)
     
     def the_button_was_clicked(self):
-        print("Clicked!")
+        new_window_title = choice(window_titles)
 
+        print(f"Setting title: {new_window_title}")
+        self.setWindowTitle(new_window_title)
 
+    def the_window_title_changed(self, window_title):
+        print(f"Window title changed: {window_title}")
+        if window_title == "Something went wrong":
+            self.button.setDisabled(True)
+            
 app = QApplication(sys.argv)
 
 # Create a Qt widget, which will be the top window
