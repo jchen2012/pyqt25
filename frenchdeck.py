8bd1a67764f6e1a3068d14a9c182e4b155aeab48
Ji Chen <jchen@tactilemedical.com>
2025-12-17 14:18:36 -0600

    Added the frenchdeck

---
+++ frenchdeck.py
@@ -0,0 +1,35 @@
+#FrenchDeck:
+#Example 1-1. A deck as a sequence of playing cards 
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                                        for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+deck = FrenchDeck()
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+for card in sorted(deck, key=spades_high):  # doctest: +ELLIPSIS
+    print(card)
+
+
+#Ramalho, Luciano. Fluent Python: Clear, Concise, and Effective Programming (p. 7). O'Reilly Media. Kindle Edition. 
+#Ramalho, Luciano. Fluent Python: Clear, Concise, and Effective Programming (p. 7). O'Reilly Media. Kindle Edition. 
+#Ramalho, Luciano. Fluent Python: Clear, Concise, and Effective Programming (p. 5). O'Reilly Media. Kindle Edition. 
